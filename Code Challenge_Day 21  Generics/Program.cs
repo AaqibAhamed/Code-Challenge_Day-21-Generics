34a2ad92c8a391fb83af1bb73d10fcf91899b268
AaqibGitBit <ahamedaaqib96@gmail.com>
2020-09-19 09:13:43 +0530

    Add project files.

---
+++ Code Challenge_Day 21  Generics/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Code_Challenge_Day_21__Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            int[] intArray = new int[n];
+            for(int i=0;i<n;i++)
+            {
+                intArray[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            n = Convert.ToInt32(Console.ReadLine());
+            string[] stringArray = new string[n];
+            for(int j=0;j<n;j++)
+            {
+                stringArray[j] = Console.ReadLine();
+            }
+
+            PrintArray<Int32>(intArray);
+            PrintArray<string>(stringArray);
+
+            
+        }
+
+        private static void PrintArray<T>(T[] Array)
+        {
+            Console.WriteLine();
+
+            foreach(T item in Array)
+            {               
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
